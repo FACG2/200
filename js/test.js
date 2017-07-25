1c3f4d4d15cca440b95143770cb609b41f39ef5d
walidmash <waleedmash2@gmail.com>
2017-07-25 15:54:29 +0300

    main structure

---
+++ js/test.js
@@ -0,0 +1,54 @@
+// var test = require('tape');
+// var todoFunctions = require('../logic');
+//
+// // Delete toDo test
+// test('deleteTodo test', function(t) {
+//   var actual = todoFunctions.deleteTodo(todoList2, 1);
+//   var expected =  [
+//     {
+//       id: 2 ,
+//       description : "second one",
+//       done : false
+//     },
+//     {
+//       id: 4 ,
+//       description : "4th one",
+//       done : false
+//     },
+//     {
+//       id: 3 ,
+//       description : "3rd one",
+//       done : true
+//     }
+//   ];
+//   t.deepEqual(actual, expected, 'deleteTodo function test pass');
+//   t.end();
+// });
+// // Mark toDo test
+// test('markTodo function test', function(t) {
+//   var actual = todoFunctions.markTodo(todoList2, 1);
+//   var expected =  [
+//     {
+//       id: 1 ,
+//       description : "first one",
+//       done : false
+//     },
+//     {
+//       id: 2 ,
+//       description : "second one",
+//       done : false
+//     },
+//     {
+//       id: 4 ,
+//       description : "4th one",
+//       done : false
+//     },
+//     {
+//       id: 3 ,
+//       description : "3rd one",
+//       done : true
+//     }
+//   ];
+//   t.deepEqual(actual, expected, 'markTodo function test pass');
+//
+// });
