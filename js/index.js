b52ff7d7275203249256f41914e9d4f1e1d13515
mahmne <mahmod_mne@hotmail.com>
2017-07-26 10:21:59 +0300

    write function getGif

--- js/index.js
+++ js/index.js
@@ -8,6 +8,21 @@ function getActor(actorName) {
 }
 
 // getGif for Mahmoud
-function getGif(movieName) {
+function getGif(movieName,fn) {
 
+  var result = [];
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var myJSONRemote = JSON.parse(xhr.responseText);
+      fn(myJSONRemote);
+      myJSONRemote.data.map(function(myData) {
+        myImgs.push(myData.embed_url);
+      });
+      return myImgs;
+    }
+  };
+
+  xhr.open("GET", "http://api.giphy.com/v1/gifs/search?q="+ movieName.replace(/ /g , '+') + "&api_key=57d2a87757c84c429f37713f2339c68a&limit=4", true);
+  xhr.send();
 }
