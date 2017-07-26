f1d259d4c5d51ed1c082f029cca474ab609d8016
walidmash <waleedmash2@gmail.com>
2017-07-26 15:11:26 +0300

    use call backs in dom function

--- js/index.js
+++ js/index.js
@@ -10,18 +10,25 @@
       body.classList.remove('goCenter');
       var actorName = event.target.firstElementChild.value;
       event.target.firstElementChild.value = "";
-      var actor = getActor(actorName);
-      console.log(actor);
-      console.log(actor.profile);
-      document.getElementsByClassName('actorName')[0].textContent = actorName;
-      var age = getAge(actor.profile);
-      document.getElementById('lookLike')= "He looks "+ age +" years old";
+
+      setTimeout(function(){
+          getActor(actorName, function(actor) {
+            document.getElementsByClassName('actorName')[0].textContent = actorName;
+            actorAvatar.setAttribute("src", actor.profile);
+             console.log(actor.profile);
+             getAge(actor.profile , function(age){
+               console.log("this is me :  ",age);
+                document.getElementById('lookLike').textContent= "He looks "+ age +" years old";
+             });
+          });
+          //
+      }, 0);
     });
   }
 })();
 
 //getActor for Hana
-function getActor(actorName) {
+function getActor(actorName , fn) {
   var xhr = new XMLHttpRequest();
   var out = {};
   var objjj = {};
@@ -29,64 +36,50 @@ function getActor(actorName) {
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4 && xhr.status == 200) {
       var result = JSON.parse(xhr.responseText);
-// console.log("heba", result);
       var known = {};
       if (result.results.length>0){
         out.status=1;
-      result.results.map(function(myData) {
+        result.results.map(function(myData) {
         known = myData.known_for;
         out.profile = "https://image.tmdb.org/t/p/w640" + myData.profile_path;
-        console.log("hana",myData.profile_path);
-      })
-
-      out.films = known.map(function(myData) {
+        })
+        out.films = known.map(function(myData) {
         objjj = {};
         objjj.avater = "https://image.tmdb.org/t/p/w640" + myData.poster_path;
         objjj.filmName = myData.original_title;
         objjj.website = "https://www.themoviedb.org/movie/" + myData.id;
         objjj.overview = myData.overview;
         return objjj;
-      })
+        });
+      }
+      else {
+        out.status=0;
+      }
+      console.log(fn);
+    fn(out);
     }
-else {
-  out.status=0;
-}
-  }
   };
   xhr.open("GET", url, true);
   xhr.send();
-  console.log(out);
   return out;
 }
 
-
-
-
-
-
-
-function getAge(profileImg) {
-
+function getAge(profileImg , fn) {
   var xhr2 = new XMLHttpRequest();
   profile = profileImg.replace(/\//g, '%2F');
   profile2 = profile.replace(/:/g, '%3A');
-  var age = 0;
-  // console.log("image",profile2);
   var url = "https://faceplusplus-faceplusplus.p.mashape.com/detection/detect?attribute=glass%2Cpose%2Cgender%2Cage%2Crace%2Csmiling&url=" + profile2;
-  console.log("link", url);
   xhr2.onreadystatechange = function() {
     if (xhr2.readyState == 4 && xhr2.status == 200) {
-      console.log(xhr2.responseText);
       var result = JSON.parse(xhr2.responseText);
-      console.log("res", result);
-      age = result.face[0].attribute.age.value;
+      var age = result.face[0].attribute.age.value;
+      fn(age);
     }
   };
   xhr2.open("GET", url, true);
   xhr2.setRequestHeader("X-Mashape-Key", "CfmtogkcYLmsh7Vd55p4dPTL1iMjp1vX9JQjsnYIIVgERw93Bk");
   xhr2.setRequestHeader("Accept", "application/json")
   xhr2.send();
-  return age;
 }
 // getGif for Mahmoud
 function getGif(movieName,fn) {
@@ -95,17 +88,16 @@ function getGif(movieName,fn) {
   xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       var myJSONRemote = JSON.parse(xhr.responseText);
-      fn(myJSONRemote);
       myJSONRemote.data.map(function(myData) {
         myImgs.push(myData.embed_url);
       });
+      fn(myImgs);
       return myImgs;
     }
   };
   xhr.open("GET", "http://api.giphy.com/v1/gifs/search?q="+ movieName.replace(/ /g , '+') + "&api_key=57d2a87757c84c429f37713f2339c68a&limit=4", true);
   xhr.send();
 }
-getActor("selena gomaz");
 // console.log(getActor("selena gomaz"));
 //
 // module.exports = getActor;
